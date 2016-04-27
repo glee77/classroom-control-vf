911f29b89a20300029cd9fabef187b2b3236266d
glee77 <glee@datarecognitioncorp.com>
2016-04-27 12:11:40 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,3 +1,14 @@
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+}
+
+
 class nginx {
 File {
 owner => 'root',
